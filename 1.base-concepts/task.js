9f55666c0685a093535a8e6624ee7c57d4778e79
daria <kotlyci@mail.ru>
2023-10-12 16:47:21 +0300

    Сделано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,44 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = (b ** 2 - 4 * a * c);
+
+	if (d > 0) {
+		let root1 = (-b + Math.sqrt(d)) / (2 * a);
+		let root2 = (-b - Math.sqrt(d)) / (2 * a);
+
+		arr.push(root1, root2);
+		console.log(arr);
+		return arr;
+
+	} else if (d === 0) {
+		let root3 = -b / (2 * a);
+
+		arr.push(root3);
+		console.log(arr);
+		return arr;
+
+	} else {
+		console.log("Корней нет");
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = parseInt(percent) / 100 / 12; //Процентная ставка
+  let c = parseInt(contribution); //начальный взнос
+  let a = parseInt(amount); //Общая стоимость
+
+  if (isNaN(p) || p < 0) {
+    return `false`;
+  } else {
+    let s = a - c;
+    let n = countMonths;
+    let pay = s * (p + (p / (((1 + p)**n) - 1)));
+    let totalAmount = (pay * n).toFixed(2);
+		  console.log(totalAmount);
+		  return +totalAmount;
+  }
 }
